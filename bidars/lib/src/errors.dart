a08d1523afef0717814b03d964bd720da063b025
halka9000stg <as.he.that.doth.serve@gmail.com>
2025-06-21 20:50:17 +0900

    add: completed error class impl. except parse error

--- bidars/lib/src/errors.dart
+++ bidars/lib/src/errors.dart
@@ -1,11 +1,34 @@
 abstract class BidarsError {}
 
-class BidarsIdentifierError extends Object implements BidarsError {}
+class BidarsTypeError extends FormatException implements BidarsError {
+  String text;
+  
+  BidarsTypeNameError(this.text);
+  
+  @override
+  String toString() => "Illegal format for typename: ${this.text}";
+}
 
-class BidarsParseError extends Object implements BidarsError{
+class BidarsIdentifierError extends FormatException implements BidarsError {
+  String text;
+  
+  BidarsIdentifierError(this.text);
+  
+  @override
+  String toString() => "Illegal format for identifier: ${this.text}";
+}
+
+class BidarsParseError extends FormatException implements BidarsError {
   
 }
 
-class BidarsIllegalValueError extends FormatException implements BidarsError{
+class BidarsIllegalValueError extends FormatException implements BidarsError {
+  BidarsType type;
+  BidarsIdent ident;
+  String text;
   
+  BidarsIllegalValueError(this.type, this.ident, this.text);
+  
+  @override
+  String toString() => "Illegal format for value of ${this.type} type (for ${this.ident}): ${this.text}";
 }
