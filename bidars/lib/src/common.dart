30fb58c7025b43547a50879b55c85ca327c40f1f
halka9000stg <as.he.that.doth.serve@gmail.com>
2025-06-21 20:48:59 +0900

    add: format checking & validation supporters

--- bidars/lib/src/common.dart
+++ bidars/lib/src/common.dart
@@ -1,28 +1,43 @@
 import "package:bidars/src/errors.dart";
 
-typedef Varidator = bool Function(String);
-typedef EnvelopeString = ({String start, String end});
+typedef UValidator<T> = bool Function(T);
+typedef Validator = UValidator<String>;
+typedef EnvelopeString = ({required String start, required String end});
 
-extension Checker on bool Function(String) {
-  String checkFor<E extends >(String input, {E Function(String)? onIllegal}){
+extension Checker on bool Validator {
+  String checkFor<E extends BidarsError>(String input, {E Function(String)? onIllegal}){
     if(this(input)) return input;
     if(onIllegal != null) {
       throw onIllegal(input);
     } else {
-      throw 
+      throw FormatException();
     }
   }
+  Validator or(Validator other) => (String input) => this(input) || other(input);
+  Validator and(Validator other) => (String input) => this(input) && other(input);
+  Validator ifThenAndU((T, U) Function(String) chomp, UValidator<T> left, UValidator<U> right) => (String input){
+    if(this(input)){
+      final(T l, U r) = chomp(input);
+      return left(l) && right(r);
+    }
+    return false;
+  }
+  Validator ifThenAnd((String, String) chomp, Validator left, Validator right) => this.ifThenAndU(chomp, left, right);
 }
 
 extension ReCheck on RegExp {
-  String checkFor<E extends >(String input, {E Function(String)? onIllegal}) => this.hasMatch.checkFor<E>(input, onIllegal: onIllegal);
+  String checkFor<E extends BidarsError>(String input, {E Function(String)? onIllegal}) => this.asVaridator().checkFor<E>(input, onIllegal: onIllegal);
+  Validator asVaridator() => this.hasMatch;
 }
 
 
 class BidarsType {
   final String name;
+  Validator expects;
 
-  BidarsType(String name): this.name = BidarsType.re.checkFor(name);
+  BidarsType(String name, {Validator? expects}):
+    this.name = BidarsType.re.checkFor(name),
+    this.expects = expects ?? (String _) => true;
 
   bool get isScalar => BidarsType.scalarTypes.contains(this.name);
   bool get isCollective => BidarsType.collectiveTypes.contains(this.name);
@@ -35,7 +50,7 @@ class BidarsType {
 
   //specials
   static BidarsType any = BidarsType("any");
-  static BidarsType refer = BidarsType("refer");
+  static BidarsType refer = BidarsType("refer", expects: ((String i) => i.startsWith("&")).ifThen((String s) => s.substring(1), BidarsIdent.re.asVaridator()));
   static BidarsType option = BidarsType("option");
   static BidarsType or = BidarsType("or");
 
